a8ecc1cd016483df17896ab1c503ca6730b92b68
Archie G <acgelonga@gmail.com>
2022-10-05 10:33:16 -0500

    Add comments

--- script.js
+++ script.js
@@ -13,11 +13,18 @@ for (let i = 0; i < navs.length; i++) {
 function createNavClickHandler(nav) {
   return (e) => {
     if (e.target.nodeName) {
+      // get the target container
       let targetContainer = document.getElementById(e.target.dataset["target"]);
+
+      // remove active class from all the nav children
       for (let i = 0; i < nav.children.length; i++) {
         nav.children[i].classList.remove("active");
       }
+
+      // add active class to this nav's parent li
       e.target.parentNode.classList.add("active");
+
+      // hide target container siblings
       for (let i = 0; i < targetContainer.parentNode.children.length; i++) {
         targetContainer.parentNode.children[i].classList.add("hidden");
       }
